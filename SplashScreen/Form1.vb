14baa9cd6c100bba82877d0f15f0fc85456bb6f8
andiaspin <andiaspin3@gmail.com>
2017-12-22 14:26:32 +0800

    Splash Screen

---
+++ SplashScreen/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        RectangleShape2.Width += 5
+        If RectangleShape2.Width >= 242 Then
+            Timer1.Dispose()
+            Me.Hide()
+            Form2.Show()
+        End If
+    End Sub
+End Class
